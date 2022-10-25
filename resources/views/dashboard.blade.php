1e89901fb8d3c8c72ce372b26e7b3682826c7f0b
varda <vardaninafianur@gmail.com>
2022-10-25 08:46:34 +0700

    p

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                             Jumlah Siswa</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $jumlah }}</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-child fa-2x text-gray-300"></i>
